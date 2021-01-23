749e9ce5f2c2b1c44d79fc22a318dcb2f897ab22
John Kenedy <john.kenedy0410@gmail.com>
2021-01-23 12:17:09 -0300

    added functionality: text to speech

--- app/src/main/java/com/canytech/texttospeech/MainActivity.kt
+++ app/src/main/java/com/canytech/texttospeech/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.canytech.texttospeech
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+
+        btnSpeak.setOnClickListener { view ->
+
+            if (etEnteredText.text.isEmpty()) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Enter a text to use the Speaker",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                speakOut(etEnteredText.text.toString())
+            }
+        }
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.US)
+
+            when (result) {
+                TextToSpeech.LANG_MISSING_DATA, TextToSpeech.LANG_NOT_SUPPORTED -> {
+                }
+            }
+        }
+    }
+
+    public override fun onDestroy() {
+
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        super.onDestroy()
+    }
+
+    private fun speakOut(text: String) {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
